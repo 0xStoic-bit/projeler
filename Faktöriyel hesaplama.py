5782e374564e4555bf68c54a2c9435d882473756
0xStoic-bit <mustafagungors0606@gmail.com>
2025-10-18 17:15:48 +0300

    Add factorial calculation script
    Implement factorial calculation based on user input.

---
+++ Faktöriyel hesaplama.py
@@ -0,0 +1,18 @@
+sayı1 = int(input('Bir sayı giriniz'))
+faktöriyel = 1
+for i in range(1,sayı1+1):
+    faktöriyel *=i
+print(f'{sayı1}! = {faktöriyel}')
+
+
+#1- Start
+#2 -set Factorial = 1
+#3- Read(Number)
+#4- if Number = 0 then
+      #print Number! = Factorial
+      #Go to Step 8
+#5- for Counter = 1 to Number step 1
+#6- Factorial = Factorial*Counter
+#7- Print (Number)! = Factorial
+#8- Print 'Program Finish'
+#9- End
